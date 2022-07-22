4bef79fdfd7b5c29cce5a72c78f74f32e7b17455
Danny Serrano <drummerextro@aol.com>
2022-07-22 16:49:48 -0400

    **Chore: adding simple objects and stars for examples. - added textures to the bg - adding an animated torus for example - updated controls ===== adding and experimenting with custom objects from maya

--- luis-3d-portfolio/main.js
+++ luis-3d-portfolio/main.js
@@ -1,13 +1,14 @@
 import './style.css'
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 //setting the scene like in a game
 const scene = new THREE.Scene();
 
 //sets the type of camera and position and perspective of it based on size of window
 
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, .1, 1000);
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, .2, 1000);
 
 //chooses the renderer and puts it into the canvas
 const renderer = new THREE.WebGL1Renderer({
@@ -51,6 +52,37 @@ scene.add(lightHelper, gridHelper)
 
 //creates orbit controls into the browser
 const controls = new OrbitControls(camera, renderer.domElement);
+
+//creates a spheracle figure 
+function addStar(){
+const geometry = new THREE.SphereGeometry(.25,24,24);
+const material = new THREE.MeshStandardMaterial ({color: 0xffffff})
+const star = new THREE.Mesh(geometry, material);
+
+//creates a math variable which randomly generates a bumber betwen a -2 to +100 spread
+const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100));
+
+//sets postion of stars
+star.position.set(x,y,z);
+
+//adds star to scene
+scene.add(star)
+}
+
+//decides how many stars added to the scene, then fill it, and add.
+Array(200).fill().forEach(addStar)
+
+
+//adding a background of some sort using a material
+const bgTexture = new THREE.TextureLoader().load('bluemoonbg.png');
+scene.background = bgTexture;
+
+
+// Instantiate a loader
+const loader = new GLTFLoader();
+
+
+
 
 //loop a animation which in this case is the scene refresh
 function animate(){
