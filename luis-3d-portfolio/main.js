f079ceee6243866dac9ae7676c466f329cbc2886
Danny Serrano <drummerextro@aol.com>
2022-07-28 19:44:23 -0400

    **Chore: Added html copy and scroll functionality into 3D objects - took out orbit, it was messing with the translate and camera positioning - messed with fov - added CSS

--- luis-3d-portfolio/main.js
+++ luis-3d-portfolio/main.js
@@ -1,25 +1,41 @@
 import './style.css'
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+//import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
+import Stats from 'three/examples/jsm/libs/stats.module'
 
 //setting the scene like in a game
 const scene = new THREE.Scene();
 
 //sets the type of camera and position and perspective of it based on size of window
 
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, .2, 1000);
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, .1, 150);
+
 
 //chooses the renderer and puts it into the canvas
-const renderer = new THREE.WebGL1Renderer({
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
   canvas: document.querySelector('#bg'),
 });
 
 //sets the size and camera position for devices
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
+document.body.appendChild(renderer.domElement)
 
+camera.position.set( 3, -3, 30 );
+//camera.position.setZ(30);
+
+
+//creates orbit controls into the browser
+// const controls = new OrbitControls(camera, renderer.domElement);
+// controls.enableDamping = true
+
+// controls.minDistance = 1;
+// controls.maxDistance = 1000;
+
+// controls.minZoom = 2;
+// controls.maxZoom = 500;
 
 //create objects and define object
 const geometry = new THREE.TorusGeometry(10,3,16,100);
@@ -50,8 +66,6 @@ const lightHelper = new THREE.PointLightHelper(pointLight)
 const gridHelper = new THREE.GridHelper(200, 50)
 scene.add(lightHelper, gridHelper)
 
-//creates orbit controls into the browser
-const controls = new OrbitControls(camera, renderer.domElement);
 
 //creates a spheracle figure 
 function addStar(){
@@ -77,24 +91,20 @@ Array(200).fill().forEach(addStar)
 const bgTexture = new THREE.TextureLoader().load('bluemoonbg.png');
 scene.background = bgTexture;
 
+//GROUP FOR O3D OBJECT
+
+// Instantiate a loader for 3d objects as a group. EACH loader is a group
+const redStaplerObject = new FBXLoader();
 //materials for objects
 const redStaplerTexture = new THREE.TextureLoader().load('objects/redStapler.png');
 
+redStaplerObject.load('objects/redStapler.fbx',
+(redstapler) => {
 
-
-// Instantiate a loader for 3d objects
-const redStaplerObject = new FBXLoader();
-
-//fbxLoader.scale.set(10,10,10);
-
-redStaplerObject.load(
-  //resource
-'objects/redStapler.fbx',
-(object) => {
-
-  object.scale.set(.15,.15,.15);
-  object.position.set(5,5,5);
-  object.traverse(function (child) {
+  redstapler.scale.set(.2,.2,.2);
+  redstapler.position.set(0,-5,-30);
+  redstapler.rotation.set(5,5,5);
+  redstapler.traverse(function (child) {
     if (child instanceof THREE.Mesh) {
 
         // apply texture
@@ -102,28 +112,105 @@ redStaplerObject.load(
         child.material.needsUpdate = true;
     }
 });
-  scene.add( object )
-  },
-)
+  scene.add( redstapler )
+  
+  
+//redStaplerObject.position.z = 30;
+//redStaplerObject.position.setx(-10);
+
+})
+
+//geometry.position.z =30;
+//geometry.position.setx(-10);
+
+
+function moveCamera(){
+
+  const t = document.body.getBoundingClientRect().top;
+
+//redStaplerObject.child.rotation.x += .05;
+//redStaplerObject.child.y += .075;
+//redStaplerObject.child.z += .05;
+
+donut.rotation.z += .05;
+donut.rotation.y += .075;
+donut.rotation.x += .05;
+
+camera.position.z = t * -.05; 
+camera.position.y = t * -0.02;
+camera.position.x = t * 0.00;
+
+}
+
+//document.body.onscroll = 
+
+document.body.onscroll = function () {
+  //calculate the current scroll progress as a percentage
+  scrollPercent =
+      ((document.documentElement.scrollTop || document.body.scrollTop) /
+          ((document.documentElement.scrollHeight ||
+              document.body.scrollHeight) -
+              document.documentElement.clientHeight)) *
+          100;
+  document.getElementById('scrollProgress').innerText =
+      'Scroll Progress : ' + scrollPercent.toFixed(2);
+
+moveCamera();
+
+    };
+
+
+console.log(camera.position);
+
+function scalePercent(start, end) {
+  return (scrollPercent - start) / (end - start);
+}
+
+function playScrollAnimations() {
+  animationScripts.forEach(function (a) {
+      if (scrollPercent >= a.start && scrollPercent < a.end) {
+          a.func();
+      }
+  });
+}
+var scrollPercent = 0;
+
+
+
+
+
+const stats = Stats()
+document.body.appendChild(stats.dom)
+
+
+
+// function updateCamera(ev) {
+//   let div1 = document.getElementById("div1");
+// camera.position.z = -1.5 + window.scrollY / 250.0;
+// }
+
+// window.addEventListener("scroll", updateCamera);
+
 
 
 //loop a animation which in this case is the scene refresh
 function animate(){
   requestAnimationFrame(animate);
 
-  //rotation of object
+  //rotation of a simple object
   donut.rotation.x += 0.02;
   donut.rotation.y += 0.005;
-  donut.rotation.z += 0.02;
-
+  donut.rotation.z += -0.02;
   
 
-//updates and makign sure canges are reflected onto the DOM
-controls.update();
 
+//updates and makign sure canges are reflected onto the DOM
+stats.update()
   //draws onto the screen
 renderer.render(scene, camera);
 }
 
 //calls the animate function which refreshes the camera
 animate();
+
+
